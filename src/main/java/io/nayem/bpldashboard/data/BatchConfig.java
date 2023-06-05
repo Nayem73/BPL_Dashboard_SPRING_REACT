555f53d67a93dfacfac3a8e97c0669c43ae49227
Nayem73 <nayem73@gmail.com>
2023-06-05 21:37:47 +0600

    added winning team full name in CSV file, added team.java, edit JobCompletionNotificationListener to count the wins for each team. Alhamdulillah now we have all the data ready to build the apis

--- src/main/java/io/nayem/bpldashboard/data/BatchConfig.java
+++ src/main/java/io/nayem/bpldashboard/data/BatchConfig.java
@@ -1,5 +1,85 @@
 package io.nayem.bpldashboard.data;
 
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import io.nayem.bpldashboard.data.model.Match;
+
+
+@Configuration
+@EnableBatchProcessing
 public class BatchConfig {
-    
+
+    private final String[] FIELD_NAMES = new String[] { 
+        "id","season","match_no","date","team_1","team_1_score","team_2","team_2_score","player_of_match","toss_winner","toss_decision","winner","venue","city","win_by_wickets","win_by_runs","result","umpire_1","umpire_2"
+    };
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public FlatFileItemReader<MatchInput> reader() {
+        return new FlatFileItemReaderBuilder<MatchInput>().name("MatchItemReader")
+                .resource(new ClassPathResource("match-data.csv")).delimited().names(FIELD_NAMES)
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
+                    {
+                        setTargetType(MatchInput.class);
+                    }
+                }).build();
+    }
+
+    @Bean
+    public MatchDataProcessor processor() {
+        return new MatchDataProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Match> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Match>()
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("INSERT INTO match (id,season,match_no,date,team1,team1score,team2,team2score,player_of_match,toss_winner,toss_decision,match_winner,venue,city,win_by_wickets,win_by_runs,result,umpire1,umpire2) "
+                        + " VALUES (:id,:season,:matchNo,:date,:team1,:team1Score,:team2,:team2Score,:playerOfMatch,:tossWinner,:tossDecision,:matchWinner,:venue,:city,:winByWickets,:winByRuns,:result,:umpire1,:umpire2)")
+                .dataSource(dataSource).build();
+    }
+
+    @Bean
+    public Job importUserJob(JobCompletionNotificationListener listener, Step step1) {
+        return jobBuilderFactory
+            .get("importUserJob")
+            .incrementer(new RunIdIncrementer())
+            .listener(listener)
+            .flow(step1)
+            .end()
+            .build();
+    }
+
+    @Bean
+    public Step step1(JdbcBatchItemWriter<Match> writer) {
+        return stepBuilderFactory
+            .get("step1")
+            .<MatchInput, Match>chunk(10)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer)
+            .build();
+    }
 }
---
+++ src/main/java/io/nayem/bpldashboard/data/model/Match.java
@@ -0,0 +1,152 @@
+package io.nayem.bpldashboard.data.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Match {
+    @Id
+    private long id;
+    private String season;
+    private String matchNo;
+    private String date;
+    private String team1;
+    private String team1Score;
+    private String team2;
+    private String team2Score;
+    private String playerOfMatch;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String venue;
+    private String city;
+    private String winByWickets;
+    private String winByRuns;
+    private String result;
+    private String umpire1;
+    private String umpire2;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getSeason() {
+        return season;
+    }
+    public void setSeason(String season) {
+        this.season = season;
+    }
+    public String getMatchNo() {
+        return matchNo;
+    }
+    public void setMatchNo(String matchNo) {
+        this.matchNo = matchNo;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getTeam1() {
+        return team1;
+    }
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+    public String getTeam1Score() {
+        return team1Score;
+    }
+    public void setTeam1Score(String team1Score) {
+        this.team1Score = team1Score;
+    }
+    public String getTeam2() {
+        return team2;
+    }
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+    public String getTeam2Score() {
+        return team2Score;
+    }
+    public void setTeam2Score(String team2Score) {
+        this.team2Score = team2Score;
+    }
+    public String getPlayerOfMatch() {
+        return playerOfMatch;
+    }
+    public void setPlayerOfMatch(String playerOfMatch) {
+        this.playerOfMatch = playerOfMatch;
+    }
+    public String getTossWinner() {
+        return tossWinner;
+    }
+    public void setTossWinner(String tossWinner) {
+        this.tossWinner = tossWinner;
+    }
+    public String getTossDecision() {
+        return tossDecision;
+    }
+    public void setTossDecision(String tossDecision) {
+        this.tossDecision = tossDecision;
+    }
+    public String getMatchWinner() {
+        return matchWinner;
+    }
+    public void setMatchWinner(String matchWinner) {
+        this.matchWinner = matchWinner;
+    }
+    public String getVenue() {
+        return venue;
+    }
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getWinByWickets() {
+        return winByWickets;
+    }
+    public void setWinByWickets(String winByWickets) {
+        this.winByWickets = winByWickets;
+    }
+    public String getWinByRuns() {
+        return winByRuns;
+    }
+    public void setWinByRuns(String winByRuns) {
+        this.winByRuns = winByRuns;
+    }
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+    public String getUmpire1() {
+        return umpire1;
+    }
+    public void setUmpire1(String umpire1) {
+        this.umpire1 = umpire1;
+    }
+    public String getUmpire2() {
+        return umpire2;
+    }
+    public void setUmpire2(String umpire2) {
+        this.umpire2 = umpire2;
+    }
+    
+
+    
+   
+    
+}
