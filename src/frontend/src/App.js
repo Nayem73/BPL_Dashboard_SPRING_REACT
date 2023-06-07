b64ef3642081fe0532057c1d19bb1f892195dafb
Nayem73 <nayem73@gmail.com>
2023-06-07 16:52:31 +0600

    Alhamdulillah TeamPage.js ready after adding routes from App.js and removing the hardcoded path with dynamic path link in TeamPage.js and updating bot MatchDetailCard.js and MatchSmallCard.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-     <TeamPage  />
+      <Router>
+        <Routes>
+          <Route path="/teams/:teamName" element={<TeamPage />}> </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
