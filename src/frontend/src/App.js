bb4ff45ca372b8bf2e22e5f5507df5e88fc03d98
Nayem73 <nayem73@gmail.com>
2023-06-07 17:53:40 +0600

    added MatchPage.js in hardcoded mode, now need Spring to create api with required match data

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />}> </Route>
           <Route path="/teams/:teamName" element={<TeamPage />}> </Route>
         </Routes>
       </Router>
