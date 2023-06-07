fe6edf12b0ebaedcac4f4e17fbf8a325814fac06
Nayem73 <nayem73@gmail.com>
2023-06-07 21:22:39 +0600

    connected with match api by MatchPage.js in react in a hardcoded way

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -4,11 +4,28 @@ import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
-    
+  
+  const [matches, setMatches] = useState([]);
+  const teamName = "Comilla Victorians";
 
-    return (
+  useEffect(
+    () => {
+        const fetchMatches = async() => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=2022`);
+            const data = await response.json();
+            setMatches(data);
+        };
+        fetchMatches();
+
+
+    }, []
+  );
+
+  return (
     <div className="MatchPage">
         <h1>Match Page</h1>
+        {matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)}
+        
     </div>
   );
 }
