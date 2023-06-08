912f1f14ff8b4381604797717d5c5e43759c781a
Nayem73 <nayem73@gmail.com>
2023-06-08 14:59:57 +0600

    Alhamdulillah team page is almost ready

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,18 +1,41 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
 
+import "./MatchDetailCard.scss";
+
 export const MatchDetailCard = ({teamName, match}) => {
     if (!match) return null;
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
     return (
         <div className="MatchDetailCard">
-        <h3>Latest Matches</h3>
-        <h1>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
-        <h2>{match.date}</h2>
-        <h3>at {match.venue}</h3>
-        <h3>{match.matchWinner} won by {match.result}</h3>
+        <div>
+          <span className="vs">vs</span>
+          <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
 
+          <h2 className="match-date">{match.date}</h2>
+          <h3 className="match-venue">at {match.venue}</h3>
+          <h3 className="match-result">{match.matchWinner} won by {match.result}</h3>
+        </div>
+
+        <div className="additional-details">
+          <h3>Match No</h3>
+          <p>{match.matchNo} Match of BPL Season {match.season}</p>
+          <h3>City</h3>
+          <p>In {match.city} City</p>
+          <h3>Toss</h3>
+          <p>{match.tossWinner} won the toss and chose to {match.tossDecision}</p>
+
+          <h3>First Innings</h3>
+          <p>{match.team1} scores {match.team1Score}</p>
+          <h3>Second Innings</h3>
+          <p>{match.team2} scores {match.team2Score}</p>
+          <h3>Player of the Match</h3>
+          <p>{match.playerOfMatch}</p>
+          <h3>Umpires</h3>
+          <p>{match.umpire1}, {match.umpire2}</p>
+          
+        </div>
 
         </div>
   );
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
+import './TeamPage.scss';
+
 export const TeamPage = () => {
     
     const [team, setTeam] = useState({matches: []});
@@ -27,9 +29,24 @@ export const TeamPage = () => {
   
     return (
     <div className="TeamPage">
-     <h1>{team.teamName}</h1>
-     <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+    <div className="team-name-section">
+        <h1 className="team-name">{team.teamName}</h1>
+    </div>
+    <div className="win-loss-section">
+        Wins / Losses
+    </div>
+    <div className="match-detail-section">
+        <h3>Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+    </div>
+     
      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
+
+    <div>
+        <a href="#">More</a>
+    </div>
+
+
 
     </div>
   );
