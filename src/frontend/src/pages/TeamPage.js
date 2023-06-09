222f8151d9bce0cb3fcff555c1c4ee88c9b3bdc6
Nayem73 <nayem73@gmail.com>
2023-06-09 11:24:15 +0600

    Alhamdulillah completed the team page including coloring, pie chart and styling and aligning and padding

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
+import { PieChart } from 'react-minimal-pie-chart';
+
 import './TeamPage.scss';
 
 export const TeamPage = () => {
@@ -35,6 +37,12 @@ export const TeamPage = () => {
     </div>
     <div className="win-loss-section">
         Wins / Losses
+        <PieChart
+            data={[
+                { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+                { title: 'Wins', value: team.totalWins, color: '#4da375' },
+            ]}
+        />
     </div>
     <div className="match-detail-section">
         {/* <h3>Latest Matches</h3> */}
@@ -44,7 +52,7 @@ export const TeamPage = () => {
      {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName} match={match}/>)}
 
     <div className="more-link">
-        <a href="#">More</a>
+        <a href="#">More ></a>
     </div>
 
 
