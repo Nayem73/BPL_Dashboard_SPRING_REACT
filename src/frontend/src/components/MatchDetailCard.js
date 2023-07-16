18dcb17e7ab58354030991b600359d0ff285e82f
Nayem73 <nayem73@gmail.com>
2023-07-16 22:30:07 +0600

    for Live Score

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -8,6 +8,9 @@ export const MatchDetailCard = ({teamName, match}) => {
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     const otherTeamRoute = `/teams/${otherTeam}`;
     const isMatchWon = teamName == match.matchWinner;
+    const isLive = match.playerOfMatch;
+    const isTeam2Score = match.team2Score;
+
     return (
         <div className={isMatchWon ? 'MatchDetailCard won-card' : 'MatchDetailCard lost-card'}>
         <div>
@@ -16,7 +19,26 @@ export const MatchDetailCard = ({teamName, match}) => {
 
           <h2 className="match-date">{match.date}</h2>
           <h3 className="match-venue">at {match.venue}</h3>
-          <h3 className="match-result">{match.matchWinner} won by {match.result}</h3>
+          
+          {isLive == 'live' ? (
+            isTeam2Score != '0/0' ? (
+              <div>
+                <h3 className="match-result">Target: {match.team1Score}</h3>
+                <h3 className="match-result">{match.team2} score: {match.team2Score}</h3>
+                <h3 className="match-result">Overs: {match.winByWickets}</h3>
+              </div>
+              ) : (
+                
+                <div>
+                  <h3 className="match-result">{match.team1} score: {match.team1Score}</h3>
+                  <h3 className="match-result">Overs: {match.winByWickets}</h3>
+                </div>
+            )
+
+          ): (
+            <h3 className="match-result">{match.matchWinner} won by {match.result}</h3>                 
+          )
+          }
         </div>
 
         <div className="additional-details">
@@ -31,8 +53,17 @@ export const MatchDetailCard = ({teamName, match}) => {
           <p>{match.team1} scores {match.team1Score}</p>
           <h3>Second Innings</h3>
           <p>{match.team2} scores {match.team2Score}</p>
-          <h3>Player of the Match</h3>
-          <p>{match.playerOfMatch}</p>
+
+          {isLive=='live' ? (
+            null
+          ):
+          (
+            <div>
+              <h3>Player of the Match</h3>
+              <p>{match.playerOfMatch}</p>
+            </div>
+          )}
+
           <h3>Umpires</h3>
           <p>{match.umpire1}, {match.umpire2}</p>
           
